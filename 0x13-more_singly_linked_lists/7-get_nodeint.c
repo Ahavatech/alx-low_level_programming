f46ace031f74a0a5fd3b428d52aa9bfa996f7920
Ahavatech <ahavatechng@gmail.com>
2023-04-29 10:11:40 +0100

    :betty fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list.
-  */
+ * @head: pointer with the first node.
+ * @index: the index of the node.
+ * Return: Adress of the node that index
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *p;
